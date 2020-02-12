62d1b4f203e2c4db885c5f7d3a41d58f2e9128b2
thaduri-karunakar <reddy.karnakar4@gmail.com>
2020-02-12 12:44:30 +0800

    Remove duplicated directory

--- linux_uninstall_global_variable.py
+++
@@ -1,4 +0,0 @@
-ip = ['10.17.171.175']
-port = '22'
-username="root"
-password="interOP@123sys"
--- test.py
+++
@@ -1,5 +0,0 @@
-robotList = ['myChinarobot','myBhutanrobot','myBurmarobot','myIndiarobot','myJapanrobot','myNKorearobot',
-             'mySKorearobot','mySrilankarobot', 'myVietnamrobot', 'myIranrobot', 'myIraqrobot', 'myIsraelrobot',
-             'myJordanrobot','myTaiwanrobot', 'myOmanrobot', 'myNepalrobot', 'myQatarrobot', 'myYemenrobot','myThailandrobot']
-for i in robotList:
-    print (i)
